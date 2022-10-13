77330cae5c965f33e9be343b3c38f7e5f618542b
Nathan Hallez <nathan.hallez.etu@univ-lille.fr>
2022-10-13 21:20:58 +0200

    Ajout du projet maven
    Le .project et .classpath ont été indexés et ajoutés au .gitignore

---
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/AppTest.java
@@ -0,0 +1,16 @@
+package fr.univlille.iutinfo.s3_02.belamcanda;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
