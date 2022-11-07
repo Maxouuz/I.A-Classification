15156eaf8a12356f051b3f9ca80023395c7fb0a9
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-11-07 14:20:55 +0100

    correction package point + creation pokemonpoint

---
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/category/ICategory.java
@@ -0,0 +1,4 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.category;
+
+public interface ICategory {
+}
--- src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/point/Point.java
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/point/Point.java
@@ -1,5 +1,9 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.point;
+
+import fr.univlille.iutinfo.s3_02.belamcanda.category.ICategory;
+
 public abstract class Point{
-    public Object getValue(Colonne col);
-    public Double getNormalizedValue(Colonne col);
-    public ICategory getCategory();
+    public abstract Object getValue(Column col);
+    public abstract Double getNormalizedValue(Column col);
+    public abstract ICategory getCategory();
 }
