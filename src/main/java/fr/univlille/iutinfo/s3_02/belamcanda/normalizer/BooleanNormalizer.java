84f7d652280d399a5c0ffa47a393274efaf9e902
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-11-09 10:20:37 +0100

    Ajout BooleanNormalizer.java

--- src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/normalizer/BooleanNormalizer.java
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/normalizer/BooleanNormalizer.java
@@ -3,7 +3,7 @@ package fr.univlille.iutinfo.s3_02.belamcanda.normalizer;
 public class BooleanNormalizer implements IValueNormalizer{
     @Override
     public double normalize(Object value, Amplitude amplitude) {
-        return 0;
+        return ((boolean) value) ? 1.0 : 0.0;
     }
 
 }
