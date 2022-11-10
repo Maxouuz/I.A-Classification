684f411f192db65e13b254d92757d7f1893ead5a
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-11-10 09:04:58 +0100

    generic csv loader

---
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/loader/CSVLoaderTest.java
@@ -0,0 +1,21 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.loader;
+
+import fr.univlille.iutinfo.s3_02.belamcanda.beans.PokemonBean;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVLoaderTest {
+    final String DATA_PATH = System.getProperty("user.dir") + File.separator + "data" + File.separator ;
+    @Test
+    public void loader_should_load_all_lines() throws IOException {
+        String dataPath = DATA_PATH + "pokemon_test.csv";
+        List<PokemonBean> list = new CSVLoader<>().loadFromFile((Class)PokemonBean.class, dataPath, ',');
+        assertEquals(274, list.size());
+
+    }
+}
