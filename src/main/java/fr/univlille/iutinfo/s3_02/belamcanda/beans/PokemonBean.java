9f846e96f22a42e1d7490fb8b901cbacb0ccfc18
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-11-10 09:10:05 +0100

    pokemonBean -> pokemonPoint encapsule bean et extends Point

--- src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/beans/PokemonBean.java
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/beans/PokemonBean.java
@@ -1,37 +1,35 @@
 package fr.univlille.iutinfo.s3_02.belamcanda.beans;
 
 import com.opencsv.bean.CsvBindByName;
-import fr.univlille.iutinfo.s3_02.belamcanda.Column;
-import fr.univlille.iutinfo.s3_02.belamcanda.point.Point;
 import fr.univlille.iutinfo.s3_02.belamcanda.PokemonType;
 
-public class PokemonBean extends Point {
+public class PokemonBean {
 	@CsvBindByName(column = "name")
-	private String nom;
+	protected String nom;
 	@CsvBindByName(column = "attack")
-	private int attack;
+	protected int attack;
 	@CsvBindByName(column = "base_egg_steps")
-	private int baseEggSteps;
+	protected int baseEggSteps;
 	@CsvBindByName(column = "capture_rate")
-	private double captureRate;
+	protected double captureRate;
 	@CsvBindByName(column = "defense")
-	private int defense;
+	protected int defense;
 	@CsvBindByName(column = "experience_growth")
-	private int experienceGrowth;
+	protected int experienceGrowth;
 	@CsvBindByName(column = "hp")
-	private int hp;
+	protected int hp;
 	@CsvBindByName(column = "sp_attack")
-	private int spAttack;
+	protected int spAttack;
 	@CsvBindByName(column = "sp_defense")
-	private int spDefense;
+	protected int spDefense;
 	@CsvBindByName(column = "type")
-	private PokemonType pokemonType;
+	protected PokemonType pokemonType;
 	@CsvBindByName(column = "type2")
-	private PokemonType pokemonType2;
+	protected PokemonType pokemonType2;
 	@CsvBindByName(column = "speed")
-	private double speed;
+	protected double speed;
 	@CsvBindByName(column = "is_legendary")
-	private boolean isLegendary;
+	protected boolean isLegendary;
 
 	@Override
 	public String toString() {
@@ -41,8 +39,4 @@ public class PokemonBean extends Point {
 				+ ", speed=" + speed + ", isLegendary=" + isLegendary + "]";
 	}
 
-	@Override
-	public Object getValue(Column col) {
-		return null;
-	}
 }
---
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/point/PokemonPoint.java
@@ -0,0 +1,16 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.point;
+
+import fr.univlille.iutinfo.s3_02.belamcanda.Column;
+import fr.univlille.iutinfo.s3_02.belamcanda.beans.PokemonBean;
+
+public class PokemonPoint extends Point{
+    PokemonBean bean;
+    public PokemonPoint(PokemonBean bean) {
+        this.bean = bean;
+    }
+
+    @Override
+    public Object getValue(Column col) {
+        return null;
+    }
+}
