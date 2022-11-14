983b4c9c60dc21c883d35bcd97f1805760d2d689
Nathan Hallez <nathan.hallez.etu@univ-lille.fr>
2022-11-14 15:47:07 +0100

    Suppression de commentaires

--- src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/model/Column.java
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/model/Column.java
@@ -3,14 +3,6 @@ package fr.univlille.iutinfo.s3_02.belamcanda.model;
 import fr.univlille.iutinfo.s3_02.belamcanda.model.normalizer.Amplitude;
 import fr.univlille.iutinfo.s3_02.belamcanda.model.normalizer.IValueNormalizer;
 
-/**
- * Decrit une <i>colonne</i> d'un DataSet.
- * Une colonne a un nom, son DataSet parent et un normaliseur optionnel.
- * Un normaliseur traduit toutes les valeurs possibles de la colonne (dans son
- * DataSet) en une valeur entre 0 et 1. Seule les colonnes <i>normalisables</i>
- * (qui ont un normaliseur) peuvent etre utilisees comme axe dans le nuage de
- * points.
- */
 public class Column implements Observer {
 	private MVCModel dataset;
 	private final String name;
@@ -28,26 +20,16 @@ public class Column implements Observer {
 	public Column(String name, IValueNormalizer normalizer) {
 		this(name, 1.0, normalizer);
 	}
-	/**
-	 * Recupere la valeur de cette colonne dans la donnee en parametre, puis
-	 * normalise cette valeur) entre 0 et 1) et la retourne normalisee. Si la
-	 * colonne n'est pas normalisable, le comportement n'est pas definit.
-	 */
+
 	public double getNormalizedValue(Point point) {
 		Object denormalizedValue = point.getValue(this);
 		return normalizer.normalize(denormalizedValue, amplitude);
 	}
 
-	/**
-	 * Retourne le nom de la colonne.
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * Permet de déterminer le dataset qui contient la colonne
-	 */
 	public void setDataset(MVCModel dataset) {
 		this.dataset = dataset;
 		if (normalizer.needAmplitude()) {
@@ -59,29 +41,20 @@ public class Column implements Observer {
 		dataset.attach(this);
 	}
 
-	/**
-	 * Retourne le DataSet auquel cette colonne appartient.
-	 */
+
 	public IDataset getDataset() {
 		return dataset;
 	}
 
-	/**
-	 * Indique si cette colonne est normalisable (a un <i>normaliseur</i>).
-	 */
 	public boolean isNormalizable() {
 		return normalizer != null;
 	}
 
-	/**
-	 * Permet de donner un poids à cette colonne
-	 */
+
 	public void setWeight(Double weight) {
 		this.weight = weight;
 	}
-	/**
-	 * Retourne le poids de la colonne
-	 */
+
 	public double getWeight() {
 		return weight;
 	}
