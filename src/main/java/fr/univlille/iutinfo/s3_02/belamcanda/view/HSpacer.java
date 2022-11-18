c5537efdd44207ae0436c8be492c86ba54bc31bf
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-11-18 20:40:50 +0100

    création de spacer verticaux et horizontaux pour faire des alignements ihm

---
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/view/HSpacer.java
@@ -0,0 +1,11 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.view;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+
+public class HSpacer extends Region {
+    public HSpacer(){
+        HBox.setHgrow(this, Priority.ALWAYS);
+    }
+}
---
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/view/VSpacer.java
@@ -0,0 +1,11 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.view;
+
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+public class VSpacer extends Region {
+    public VSpacer(){
+        VBox.setVgrow(this, Priority.ALWAYS);
+    }
+}
