b31b27da7fc5ce0cbadaeb5dbf1b662f7516a96c
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-11-19 19:55:02 +0100

    Ajour du test prouvant que les distances se rapproche de 0 lorsque les points sont proche, et s'éloigne de 0 quand les points ne se ressemble pas du tout -> ManhattanDistanceTest

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestManhattanDistance.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestManhattanDistance.java
@@ -27,4 +27,18 @@ public class TestManhattanDistance {
         assertEquals(0., manhattan.distance(load.createModelFromFile(dataPath), point1, point1Bis));
     }
 
+    @Test
+    void points_are_more_or_less_close_to_zero_according_to_their_resemblance() throws IOException {
+        String dataPath = DATA_PATH + "iris.csv";
+
+        IrisPoint point1 = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
+        IrisPoint point2 = new IrisPoint(0., 0., 0., 0., IrisVariety.VIRGINICA);
+        IrisPoint point3 = new IrisPoint(1., 1., 1., 0.9, IrisVariety.SETOSA);
+
+        ManhattanDistance manhattan = new ManhattanDistance();
+        IrisLoader load = new IrisLoader();
+        assertEquals(1.2806026365348397, manhattan.distance(load.createModelFromFile(dataPath), point1, point2));
+        assertEquals(0.04166666666666663, manhattan.distance(load.createModelFromFile(dataPath), point1, point3));
+
+    }
 }
