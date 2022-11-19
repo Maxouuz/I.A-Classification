e2f4099c204b206f20d41eb538282ed5a55e9993
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-11-19 19:45:32 +0100

    Mise en place du test dans EuclidianDistance permettant de prouver que plus les points se ressemble plus leurs distances est proche de 0

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestEuclidianDistance.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestEuclidianDistance.java
@@ -28,4 +28,18 @@ public class TestEuclidianDistance {
         IrisLoader load = new IrisLoader();
         assertEquals(0., euclid.distance(load.createModelFromFile(dataPath), point1, point1Bis));
     }
+
+    @Test
+    void points_are_more_or_less_close_to_zero_according_to_their_resemblance() throws IOException {
+        String dataPath = DATA_PATH + "iris.csv";
+
+        IrisPoint point1 = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
+        IrisPoint point2 = new IrisPoint(0., 0., 0., 0., IrisVariety.VIRGINICA);
+        IrisPoint point3 = new IrisPoint(1., 1., 1., 0.9, IrisVariety.SETOSA);
+
+        EuclidianDistance euclid = new EuclidianDistance();
+        IrisLoader load = new IrisLoader();
+        assertEquals(0.6731345283372746, euclid.distance(load.createModelFromFile(dataPath), point1, point2));
+        assertEquals(0.04166666666666663, euclid.distance(load.createModelFromFile(dataPath), point1, point3));
+    }
 }
