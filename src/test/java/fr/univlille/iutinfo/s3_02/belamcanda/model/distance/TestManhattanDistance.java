613c9552a9f238ef48ff15993cb4281ebd2f46a3
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-11-19 19:51:39 +0100

    Ajout du test dans manhattan distance test prouvant que deux points totalemet égaux ont une distance de 0

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestManhattanDistance.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestManhattanDistance.java
@@ -1,49 +1,30 @@
 package fr.univlille.iutinfo.s3_02.belamcanda.model.distance;
 
+import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.IrisLoader;
+import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.IrisPoint;
+import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.qualitative_variables.IrisVariety;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestManhattanDistance {
     static double DELTA = 0.005;
+    final String DATA_PATH = System.getProperty("user.dir") + File.separator + "data" + File.separator ;
 
-    /**
-     * Test d'une propriété de la distance de manhattan. Pour plus d'informations, voir cette page:
-     * <a href="https://fr.wikipedia.org/wiki/Distance_de_Manhattan#Propriétés">Propriétés distance de manhattan</a>
-     */
-    /**
+
     @Test
-    public void test_distance_between_two_objects_equals_to_sum_of_axes_displacements() {
+    void two_identical_points_are_equal_to_zero() throws IOException {
+        String dataPath = DATA_PATH + "iris.csv";
 
-        TempObject o0 = new TempObject(0, 0, 0, 0);
-        TempObject o1 = new TempObject(1, 0, 0, 0);
-        TempObject o2 = new TempObject(1, 1, 0, 0);
-        TempObject o3 = new TempObject(0, 0, 2, 2);
-        TempObject o4 = new TempObject(0, 3, 3, 3);
-        TempObject o5 = new TempObject(4, 4, 4, 4);
+        IrisPoint point1 = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
+        IrisPoint point1Bis = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
 
-        assertEquals(1, new ManhattanDistance().distance(o0, o1, null), DELTA);
-        assertEquals(1+1, new ManhattanDistance().distance(o0, o2, null), DELTA);
-        assertEquals(2+2, new ManhattanDistance().distance(o0, o3, null), DELTA);
-        assertEquals(3+3+3, new ManhattanDistance().distance(o0, o4, null), DELTA);
-        assertEquals(4+4+4+4, new ManhattanDistance().distance(o0, o5, null), DELTA);
+        ManhattanDistance manhattan = new ManhattanDistance();
+        IrisLoader load = new IrisLoader();
+        assertEquals(0., manhattan.distance(load.createModelFromFile(dataPath), point1, point1Bis));
     }
 
-    @Test
-    public void object_with_weighted_distance() {
-        Map<String, Double> weights = new HashMap<>();
-        weights.put("Tennis", 0.);
-        weights.put("Foot", .5);
-        weights.put("Ping", 1.);
-        weights.put("Golf", 2.);
-
-        TempObject o0 = new TempObject(0, 0, 0, 0);
-        TempObject o1 = new TempObject(1, 1, 0, 0);
-        TempObject o2 = new TempObject(0, 1, 1, 0);
-        TempObject o3 = new TempObject(0, 0, 2, 2);
-        TempObject o4 = new TempObject(0, 3, 3, 3);
-        TempObject o5 = new TempObject(4, 4, 4, 4);
-
-        assertEquals(.5, new ManhattanDistance().distance(o0, o1, weights), DELTA);
-        assertEquals(.5 + 1., new ManhattanDistance().distance(o0, o2, weights), DELTA);
-        assertEquals(2 + 4, new ManhattanDistance().distance(o0, o3, weights), DELTA);
-        assertEquals(1.5 + 3 + 6, new ManhattanDistance().distance(o0, o4, weights), DELTA);
-        assertEquals(0 + 2 + 4 + 8, new ManhattanDistance().distance(o0, o5, weights), DELTA);
-    }*/
 }
