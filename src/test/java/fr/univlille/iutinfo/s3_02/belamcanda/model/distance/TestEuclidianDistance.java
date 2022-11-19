8727f54235bd1297485f89e7573f541923f9a0ba
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-11-19 20:21:04 +0100

    j'ai simplifié les tests de distance euclidienne car j'avais intancié hors des asserts et ça faisait bcp de ligne pour rien

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestEuclidianDistance.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/TestEuclidianDistance.java
@@ -24,9 +24,7 @@ public class TestEuclidianDistance {
         IrisPoint point1 = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
         IrisPoint point1Bis = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
 
-        EuclidianDistance euclid = new EuclidianDistance();
-        IrisLoader load = new IrisLoader();
-        assertEquals(0., euclid.distance(load.createModelFromFile(dataPath), point1, point1Bis));
+        assertEquals(0., new EuclidianDistance().distance(new IrisLoader().createModelFromFile(dataPath), point1, point1Bis));
     }
 
     @Test
@@ -37,9 +35,7 @@ public class TestEuclidianDistance {
         IrisPoint point2 = new IrisPoint(0., 0., 0., 0., IrisVariety.VIRGINICA);
         IrisPoint point3 = new IrisPoint(1., 1., 1., 0.9, IrisVariety.SETOSA);
 
-        EuclidianDistance euclid = new EuclidianDistance();
-        IrisLoader load = new IrisLoader();
-        assertEquals(0.6731345283372746, euclid.distance(load.createModelFromFile(dataPath), point1, point2));
-        assertEquals(0.04166666666666663, euclid.distance(load.createModelFromFile(dataPath), point1, point3));
+        assertEquals(0.04166666666666663, new EuclidianDistance().distance(new IrisLoader().createModelFromFile(dataPath), point1, point3));
+        assertEquals(0.6731345283372746, new EuclidianDistance().distance(new IrisLoader().createModelFromFile(dataPath), point1, point2));
     }
 }
