529de39b85c079678b3d7fbb71f96bac1075ce98
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-11-23 09:11:09 +0100

    random distance

---
+++ src/main/java/fr/univlille/iutinfo/s3_02/belamcanda/model/distance/RandomDistance.java
@@ -0,0 +1,14 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.model.distance;
+
+import fr.univlille.iutinfo.s3_02.belamcanda.model.MVCModel;
+import fr.univlille.iutinfo.s3_02.belamcanda.model.Point;
+
+import java.util.Random;
+
+public class RandomDistance implements Distance{
+    Random rng = new Random();
+    @Override
+    public double distance(MVCModel model, Point p1, Point p2) {
+        return rng.nextDouble();
+    }
+}
