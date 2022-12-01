7fcf517348e763ff0fe88156431acefb794dcbf1
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-12-01 20:14:24 +0100

    Ajout d'un méthode test needamplitude dans standardNormalizertest

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/normalizer/StandardScoreNormalizerTest.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/normalizer/StandardScoreNormalizerTest.java
@@ -3,12 +3,19 @@ package fr.univlille.iutinfo.s3_02.belamcanda.model.normalizer;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StandardScoreNormalizerTest {
 
     @Test
-    void Score_normalizer_is_null(){
+    void score_normalizer_is_null(){
         StandardScoreNormalizer standSc = new StandardScoreNormalizer();
         assertFalse(standSc.isNull());
+    }
+
+    @Test
+    void score_normalizer_need_amplitude(){
+        StandardScoreNormalizer standSc = new StandardScoreNormalizer();
+        assertTrue(standSc.needAmplitude());
     }
 }
