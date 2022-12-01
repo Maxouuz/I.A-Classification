26361a8d49855162561277967b074ddca0e42e82
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-12-01 19:01:32 +0100

    Test de la méthode isnull de booleanNormalizer

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/normalizer/TestBooleanNormalizer.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/normalizer/TestBooleanNormalizer.java
@@ -1,6 +1,7 @@
 package fr.univlille.iutinfo.s3_02.belamcanda.model.normalizer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,10 @@ class TestBooleanNormalizer {
     void boolean_is_correctly_normalized(){
         assertEquals(1.0,new BooleanNormalizer().normalize(true));
         assertEquals(0.0,new BooleanNormalizer().normalize(false));
+    }
+
+    @Test
+    void test_if_the_normalizer_is_null(){
+        assertFalse(new BooleanNormalizer().isNull());
     }
 }
