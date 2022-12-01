d000ab9b44cf218a0106eb63c666bec85db750bc
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-12-01 15:01:19 +0100

    Ajout de la classe test IrisPOint et de la méthode test de toLabel

---
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/implementations/iris/IrisPointTest.java
@@ -0,0 +1,17 @@
+package fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris;
+
+import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.qualitative_variables.IrisVariety;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IrisPointTest {
+
+    @Test
+    void the_name_of_label_is_correct(){
+        IrisPoint point1 = new IrisPoint(1., 1., 1., 1., IrisVariety.SETOSA);
+        String test =  String.format("Iris (l, h)\n\tSepal (%.2f, %.2f)\n\tPetal (%.2f, %.2f)", point1.sepalLength, point1.sepalWidth, point1.petalLength, point1.petalWidth);
+        assertEquals(test, point1.toLabel());
+    }
+
+}
