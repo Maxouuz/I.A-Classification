20be59c128e1c6aa0fea7d737f47b2e6c14c61d2
Maxence Stiévenard <maxence.stievenard.etu@univ-lille.fr>
2022-12-01 19:18:40 +0100

    Ajout méthode de tests dans TitanicPointTest

--- src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/implementations/titanic/TitanicPointTest.java
+++ src/test/java/fr/univlille/iutinfo/s3_02/belamcanda/model/implementations/titanic/TitanicPointTest.java
@@ -2,6 +2,7 @@ package fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.titanic;
 
 import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.IrisPoint;
 import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.iris.qualitative_variables.IrisVariety;
+import fr.univlille.iutinfo.s3_02.belamcanda.model.implementations.pokemon.PokemonPoint;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,5 +17,12 @@ public class TitanicPointTest {
         point1.fare = 100.0;
         String test = String.format("%s :\n\tClass : %d\n\tAge : %.2f\n\tFare : %.2f", point1.name, point1.pClass, point1.age, point1.fare);
         assertEquals(test, point1.toLabel());
+    }
+
+    @Test
+    void the_to_string_is_correct(){
+        TitanicPoint point1 = new TitanicPoint();
+        point1.name = "Jean";
+        assertEquals("Jean", point1.toString());
     }
 }
